c006cb84c4668e7a3bc530cb50916dd39f3d7dc2
alvin-xu <479359285@qq.com>
2013-07-09 17:02:06 +0800

    simple client receive json data

---
+++ src/cn/redarmy/service/impl/GoodServiceImpl.java
@@ -0,0 +1,119 @@
+package cn.redarmy.service.impl;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import cn.redarmy.domain.Good;
+import cn.redarmy.service.GoodService;
+
+public class GoodServiceImpl implements GoodService{
+
+	 // 获取最新的商品信息   
+    /* (non-Javadoc)  
+     * @see cn.redarmy.service.Impl.GoodService#findAll()  
+     */  
+    @Override  
+    public List<Good> findAll() {   
+        // 创建请求HttpClient客户端   
+        HttpClient httpClient = new DefaultHttpClient();   
+        // 创建请求的url   
+        String url = "http://192.168.3.101:8080/StrutsJson/csdn/listNewsGoods.action";   
+        try {   
+            // 创建请求的对象   
+            HttpGet get = new HttpGet(new URI(url));   
+            // 发送get请求   
+            HttpResponse httpResponse = httpClient.execute(get);   
+            // 如果服务成功返回响应   
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {   
+                HttpEntity entity = httpResponse.getEntity();   
+                if (entity != null) {   
+                    // 获取服务器响应的json字符串   
+                    String json = EntityUtils.toString(entity);
+                    return parseArrayJosn(json);   
+                }   
+            }   
+        } catch (Exception e) {   
+            e.printStackTrace();   
+        }   
+        return null;   
+    }   
+  
+    //解析json数组对象   
+    private List<Good> parseArrayJosn(String json) {   
+        List<Good> goods = new ArrayList<Good>();   
+        try {   
+            JSONArray array = new JSONObject(json).getJSONArray("goods");   
+            for (int i = 0; i < array.length(); i++) {   
+                JSONObject obj = array.getJSONObject(i);   
+                Good good = new Good(obj.getInt("id"), obj.getString("name"),   
+                        (float) obj.getDouble("price"));   
+                goods.add(good);   
+            }   
+        } catch (JSONException e) {   
+            e.printStackTrace();   
+        }   
+        return goods;   
+    }   
+  
+    // 获取最新的单个商品的详细信息   
+    /* (non-Javadoc)  
+     * @see cn.redarmy.service.Impl.GoodService#findById()  
+     */  
+    @Override  
+    public Good findById() {   
+        // 创建请求HttpClient客户端   
+        HttpClient httpClient = new DefaultHttpClient();   
+        // 创建请求的url   
+        String url = "http://192.168.3.101:8080/StrutsJson/csdn/findGood.action";   
+        try {   
+        	Log.d("GET", "start request");
+            // 创建请求的对象   
+            HttpGet get = new HttpGet(url);   
+            // 发送get请求   
+            HttpResponse httpResponse = httpClient.execute(get);
+            Log.d("GET","get status"+httpResponse.getStatusLine().getStatusCode());
+            
+            
+            // 如果服务成功返回响应   
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {   
+                HttpEntity entity = httpResponse.getEntity();   
+                if (entity != null) {   
+                    // 获取服务器响应的json字符串   
+                    String json = EntityUtils.toString(entity);   
+                    return parseObjJosn(json);   
+                }   
+            }   
+        } catch (Exception e) {   
+            e.printStackTrace();   
+        }   
+        return null;   
+    }   
+  
+    //解析json的单个对象   
+    private Good parseObjJosn(String json) {   
+        JSONObject obj = null;   
+        try {   
+            obj = new JSONObject(json).getJSONObject("good");   
+            Good good = new Good(obj.getInt("id"), obj.getString("name"),   
+                    (float) obj.getDouble("price"));   
+            return good;   
+        } catch (JSONException e) {   
+            e.printStackTrace();   
+        }   
+        return null;   
+    }   
+	
+}
---
+++ src/com/example/jsonclient/GoodActivity.java
@@ -0,0 +1,77 @@
+package com.example.jsonclient;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import cn.redarmy.domain.Good;
+import cn.redarmy.service.GoodService;
+import cn.redarmy.service.impl.GoodServiceImpl;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class GoodActivity extends Activity {
+	 
+	private GoodService goodService = new GoodServiceImpl(); 
+	 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_good);
+		 //解析集合   
+		      try {  
+		            List<Good> goods = goodService.findAll();  
+		            ListView listView = (ListView) this.findViewById(R.id.goods);  
+		            List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();  
+		            for (Good good : goods) {  
+		                HashMap<String, Object> item = new HashMap<String, Object>();  
+		                item.put("name", good.getName());  
+		                item.put("price", getResources().getString(R.string.price)+good.getPrice());  
+		                item.put("id", good.getId());  
+		                data.add(item);  
+		            }  
+		            SimpleAdapter adapter = new SimpleAdapter(this, data,  
+		                    R.layout.good, new String[] { "name", "price" },  
+		                    new int[] { R.id.name, R.id.price });  
+		 
+		            listView.setAdapter(adapter);  
+		        } catch (Exception e) {  
+		            Log.v("error", "网络连接失败");  
+		            e.printStackTrace();  
+		        }
+		     //解析单个对象   
+		     /*   try {   
+		        	Log.d("MAIN", "find by id");
+		            Good good = goodService.findById();   
+		            ListView listView = (ListView) this.findViewById(R.id.goods);   
+		            List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();   
+		               
+		                HashMap<String, Object> item = new HashMap<String, Object>();   
+		                item.put("name", good.getName());   
+		                item.put("price", getResources().getString(R.string.price)+good.getPrice());   
+		                item.put("id", good.getId());   
+		                data.add(item);   
+		               
+		            SimpleAdapter adapter = new SimpleAdapter(this, data,   
+		                    R.layout.good, new String[] { "name", "price" },   
+		                    new int[] { R.id.name, R.id.price });   
+		  
+		            listView.setAdapter(adapter);   
+		        } catch (Exception e) {   
+		            Log.v("error", "网络连接失败");   
+		            e.printStackTrace();   
+		        }   */
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.good, menu);
+		return true;
+	}
+
+}
